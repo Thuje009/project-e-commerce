a1e2c1dc21386e7027d774e02b9de8093eb48597
Phurin Gannedgad <BasPhurin2502@gmail.com>
2024-11-24 22:24:50 +0700

    fix error

--- components/userAccountPage/UserProfile.tsx
+++ components/userAccountPage/UserProfile.tsx
@@ -1,55 +1,59 @@
 "use client";
 
 import React from "react";
-import { RadioGroup, Radio } from "@nextui-org/react";
+import { RadioGroup, Radio, Input } from "@nextui-org/react";
 
 function UserProfile() {
   return (
-    <>
+    <div className="bg-cardBackground border-2">
+      {/* Head */}
       <div className="border-b-4">
         <h2>ข้อมูลของฉัน</h2>
         <p>จัดกาารข้อมูลส่วนตัวของคุณ</p>
       </div>
+      {/* Form */}
       <div>
-        <span>ชื่อผู้ใช้</span>
         <div>
-          <input type="text" placeholder="ชื่อผู้ใช้" />
+          <div className="flex">
+            <span className="mr-4">ชื่อผู้ใช้</span>
+            <div>
+              <Input label="ชื่อผู้ใช้" type="text" placeholder="ชื่อผู้ใช้" />
+            </div>
+          </div>
+          <div className="flex">
+            <span className="mr-4 ">
+              <p>ชื่อ</p>
+            </span>
+            <div>
+              <Input label="ชื่อ" type="text" placeholder="ชื่อ" />
+            </div>
+          </div>
+          <div>
+            <span>อีเมล</span>
+            <div>
+              <p>User@example.com</p>
+            </div>
+          </div>
+          <div>
+            <span>หมายเลขโทรศัพท์</span>
+            <div>
+              <p>0123456</p>
+            </div>
+          </div>
+          <div>
+            <span>เพศ</span>
+            <div>
+              <RadioGroup orientation="horizontal">
+                <Radio value="ชาย">ชาย</Radio>
+                <Radio value="หญิง">หญิง</Radio>
+              </RadioGroup>
+            </div>
+          </div>
         </div>
+        {/* Profile Image */}
+        <div></div>
       </div>
-      <div>
-        <span>ชื่อ</span>
-        <div>
-          <input type="text" placeholder="ชื่อผู้ใช้" />
-        </div>
-      </div>
-      <div>
-        <span>อีเมล</span>
-        <div>
-          <p>User@example.com</p>
-        </div>
-      </div>
-      <div>
-        <span>หมายเลขโทรศัพท์</span>
-        <div>
-          <p>0123456</p>
-        </div>
-      </div>
-      <div>
-        <span>เพศ</span>
-        <div>
-          <RadioGroup
-            label="Select your favorite city"
-            orientation="horizontal"
-          >
-            <Radio value="buenos-aires">Buenos Aires</Radio>
-            <Radio value="sydney">Sydney</Radio>
-            <Radio value="san-francisco">San Francisco</Radio>
-            <Radio value="london">London</Radio>
-            <Radio value="tokyo">Tokyo</Radio>
-          </RadioGroup>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
 
